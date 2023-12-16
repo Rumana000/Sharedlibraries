7cbcd6a27d579c3d73dfb8dffa028d4031c9925c
Rumana000 <153263326+Rumana000@users.noreply.github.com>
2023-12-16 21:28:12 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,11 +6,11 @@ def newMaven()
 {
   sh 'mvn package'
 }
-def newDeploy(jobname,ip,context)
+def newDeploy(workspace,ip,context)
 {
-  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+  sh "scp ${workspace}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
-def newrunSelenium(jobname)
+def newrunSelenium(workspace)
 {
-  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+  sh "java -jar ${workspace}/testing.jar"
 } 
